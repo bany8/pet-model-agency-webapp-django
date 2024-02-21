13b7ccd3622334883386215616fd56fd314afa0d
bany <bart-7@outlook.com>
2024-02-21 18:35:50 +0100

    fix setting for apps

--- pet_agency/pet_agency/settings.py
+++ pet_agency/pet_agency/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "user.apps.PollsConfig",
-    "agency.apps.PollsConfig",
+    "user.apps.UserConfig",
+    "agency.apps.AgencyConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
