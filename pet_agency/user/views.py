742a7c8685c7e4ce4d877739bef058a30acb44d8
bany <bart-7@outlook.com>
2024-02-29 13:10:59 +0100

    register done

--- pet_agency/user/views.py
+++ pet_agency/user/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def login(request):
@@ -6,7 +8,16 @@ def login(request):
 
 
 def register(request):
-    return render(request, "user/register.html")
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('agency:home_page')
+    else:
+        form = UserRegisterForm()
+    return render(request, "user/register.html", {'form': form})
 
 
 def posts(request):
