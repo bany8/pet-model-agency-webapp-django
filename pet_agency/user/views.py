684fabfb22f0c3cb8b252ddde6cffee68647a838
bany <bart-7@outlook.com>
2024-03-01 13:42:15 +0100

    profile update

--- pet_agency/user/views.py
+++ pet_agency/user/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import logout as logout_user
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def logout(request):
@@ -30,4 +31,23 @@ def posts(request):
 
 @login_required
 def profile(request):
-    return render(request, "user/profile.html")
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+
+            username = u_form.cleaned_data.get('username')
+            messages.success(request, f'Account {username} updated!')
+            return redirect('user:profile_page')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, "user/profile.html", context)
