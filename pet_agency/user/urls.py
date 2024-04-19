b571a5d164a5189123b4cfea44c34427de3ada20
bany <65906779+zBroly7@users.noreply.github.com>
2024-04-19 12:24:46 +0200

    change all views to class based

--- pet_agency/user/urls.py
+++ pet_agency/user/urls.py
@@ -1,13 +1,15 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
+from .views import RegisterView, PostsView, ProfileView
+
 from . import views
 
 app_name = "user"
 urlpatterns = [
-    path("register/", views.register, name="register_page"),
+    path("register/", RegisterView.as_view(), name="register_page"),
     path("login/", auth_views.LoginView.as_view(template_name='user/login.html'), name="login_page"),
     path("logout/", auth_views.LogoutView.as_view(template_name='user/logout.html'), name="logout_page"),
-    path("posts/", views.posts, name="posts_page"),
-    path("profile/", views.profile, name="profile_page"),
+    path("posts/", PostsView.as_view(), name="posts_page"),
+    path("profile/", ProfileView.as_view(), name="profile_page"),
 ]
--- pet_agency/user/views.py
+++ pet_agency/user/views.py
@@ -1,41 +1,51 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import logout as logout_user
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import View
 
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from agency.models import Advertisement
+from .models import Profile
 
 
-def logout(request):
-    logout_user(request)
-    return redirect('user:login_page')
+class RegisterView(View):
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, "user/register.html", {'form': form})
 
-
-def register(request):
-    if request.method == "POST":
+    def post(self, request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('user:login_page')
-    else:
-        form = UserRegisterForm()
-    return render(request, "user/register.html", {'form': form})
+        return render(request, "user/register.html", {'form': form})
 
 
-@login_required
-def posts(request):
-    context = Advertisement.objects.all()
-    return render(request, "user/posts.html", context={'advertisements': context})
+class PostsView(View):
+    def get(self, request):
+        context = Advertisement.objects.all()
+        return render(request, "user/posts.html", context={'advertisements': context})
 
 
-@login_required
-def profile(request):
-    if request.method == "POST":
+class ProfileView(LoginRequiredMixin, View):
+
+    def get(self, request):
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+        context = {
+            'u_form': u_form,
+            'p_form': p_form
+        }
+
+        return render(request, "user/profile.html", context)
+
+    def post(self, request):
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
@@ -43,13 +53,3 @@ def profile(request):
             username = u_form.cleaned_data.get('username')
             messages.success(request, f'Account {username} updated!')
             return redirect('user:profile_page')
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
-
-    return render(request, "user/profile.html", context)
